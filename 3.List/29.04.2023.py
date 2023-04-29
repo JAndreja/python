87d4b71bd5b19e2c8a2c4b86cc7ac804e8a8afad
JAndreja <andreja.j@gmail.com>
2023-04-29 13:59:21 +0200

    list update

--- 3.List/29.04.2023.py
+++ 3.List/29.04.2023.py
@@ -27,3 +27,22 @@ i = 0
 while i < len(name):
     print(f"letter {i+1}: {name[i]}")
     i += 1
+
+
+nested_list = [ [1,2,3], [4,5,6], [7,8,9] ]
+
+
+for i in nested_list:
+    for y in i:
+        print(y)
+
+i =0 
+while i < len(nested_list):
+    print(nested_list[i])
+    sub_list = nested_list[i] 
+    y=0
+    while y < len(sub_list):
+        print(sub_list[y])   
+        y += 1
+    i += 1
+
