12d5fe29527d97e523280f85f8dad190fcb3642d
JAndreja <andreja.j@gmail.com>
2023-04-30 15:17:39 +0200

    function update

--- 6.Functions/30.04.23.py
+++ 6.Functions/30.04.23.py
@@ -59,27 +59,29 @@ def sub(a,b):
 def sum_of_all(a,b,c,d):
     result = sum(a,b) + mult(c,d)
     return result
+print(sum_of_all(2,8,2,5))
 
 def sum_of_all_2(a,b):
     return sum(a,b) +mult(a,b)
+print(sum_of_all_2(2,8))
 
 def sum_of_all_3(a,b):  
     return a + b
+print(sum_of_all_3(sum(2,8),mult(2,5)))
 
 def sum_of_all_4(fn1 = sum(2,4) , fn2= mult(3,4)):
     return fn1 + fn2
-
+print(sum_of_all_4())
 
 def sum_of_all_5( a,b,c,d ,fn1 = sum , fn2= mult ):
     return fn1(a,b) + fn2(c,d)
-
+print(sum_of_all_5(5,5,5,5))
 
 def sum_of_all_6( a,b ,fn):
      return fn(a,b)
-
-print(sum_of_all(2,8,2,5))
-print(sum_of_all_2(2,8))
-print(sum_of_all_3(sum(2,8),mult(2,5)))
-print(sum_of_all_4())
-print(sum_of_all_5(5,5,5,5))
 print(sum_of_all_6(5,4,sub))
+
+
+
+
+
